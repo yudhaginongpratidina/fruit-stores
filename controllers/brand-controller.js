784d19de13bfcafbcc2bf73aacb1aa0526e6855d
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2024-05-04 09:41:19 +0700

    init brand. define route adn controller, define model and migration brand

---
+++ controllers/brand-controller.js
@@ -0,0 +1,63 @@
+const { Brand } = require("../models");
+
+class BrandController {
+
+
+    static async index(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+
+    static create(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+    static async store(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+    static async show(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+    static async update(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+    static async destroy(req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+}
+
+
+module.exports = BrandController
--- routes/web-route.js
+++ routes/web-route.js
@@ -3,11 +3,27 @@ const express = require("express");
 const router = express.Router();
 
 
-// DEFINING ROUTES
-router.get("/", (req, res) => {
-    res.json("Hello World");
-})
+// IMPORT CONTROLLERS
+const BrandController = require("../controllers/brand-controller");
 
+// =====================================================================================================================
+// DEFINING ROUTES FOR BRAND
+// ----------------------------------------------------------------------------------------------------------------------
+// GET	/brands	            Menampilkan semua brands yang ada dalam database
+// GET	/brands/add	        Menampilkan halaman form untuk menambahkan data brands
+// POST	/brands/add	        Menerima data yang dikirim dari halaman /brands/add untuk melakukan insertion
+// GET	/brands/delete/:id	Melakukan delete data brands berdasarkan id yang dikirimkan
+// GET	/brands/update/:id	Menampilkan halaman form untuk mengubah data brands dari Id
+// POST	/brands/update/:id	Menerima data yang dikirim dari halaman /brands/update/:id untuk melakukan update
+// ----------------------------------------------------------------------------------------------------------------------
+
+router.get("/brands", BrandController.index);
+router.get("/brands/add", BrandController.create);
+router.post("/brands/add", BrandController.store);
+router.get("/brands/delete/:id", BrandController.destroy);
+router.get("/brands/update/:id", BrandController.show);
+router.post("/brands/update/:id", BrandController.update);
+// =====================================================================================================================
 
 // EXPORT
 module.exports = router
