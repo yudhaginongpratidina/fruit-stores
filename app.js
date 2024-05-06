245fbc99f2d4d761b644b25fd79ef8017775060e
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2024-05-06 09:02:59 +0700

    basic styling

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set("public", path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/css")));
 
 
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{html,ejs}",
+    "/views/*.{html,ejs}",
+    "./public/**/*.{html,ejs}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
